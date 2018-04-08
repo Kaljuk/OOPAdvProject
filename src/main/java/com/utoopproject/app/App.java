64ff182b649360f8419c60edd7ffdf98b892e203
Risto Vähi <fidtal0@gmail.com>
2018-04-08 20:41:46 +0300

    Add the option to launch server or client

--- src/main/java/com/utoopproject/app/App.java
+++ src/main/java/com/utoopproject/app/App.java
@@ -1,23 +1,41 @@
 package com.utoopproject.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
         // Launch app
-        System.out.println( "Hello World!" );
+        System.out.println("Launching application, options:");
+
         // Ask if app turns into a server or client
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. launch server");
+        System.out.println("2. launch client");
 
-        // Get server or client instance
+        // Check the input and launch the selected option
+        if (sc.hasNextInt()) {
+            int option = sc.nextInt();
 
-        // Launch created instance
-        Server server = new Server();
+            if (option == 1) {
+                Server server = new Server();
+                server.startServer();
+            } else if (option == 2) {
+                sc.nextLine();
+
+                // Ask the user for server IP address
+                System.out.println("Enter the server IP address: ");
+                String serverAddress = sc.nextLine();
+
+                // Ask the user for username
+                System.out.println("Enter your username: ");
+                String username = sc.nextLine();
+
+                // Start a new client
+                Client client = new Client(serverAddress, username);
+            } else {
+                System.out.println("Invalid option!");
+            }
+        }
     }
-
-
 }
