d462eeb268eec6605266f780016bc5f763072315
Risto Vähi <fidtal0@gmail.com>
2018-04-08 20:47:48 +0300

    Broadcast received message to other clients, add sendMessage function

--- src/main/java/com/utoopproject/app/RequestHandler.java
+++ src/main/java/com/utoopproject/app/RequestHandler.java
@@ -1,49 +1,70 @@
 package com.utoopproject.app;
 
 
-import java.io.DataOutputStream;
-import java.io.DataInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 /**
  * RequestHandler
  */
 public class RequestHandler extends Thread {
-    Socket socket;
+    private Socket socket;
     private int clientID;
+    private DataOutputStream dataOut;
+    private DataInputStream dataIn;
 
     public RequestHandler(Socket requestSocket, int clientNumber) {
         super();
-        this.socket   = requestSocket;
+        this.socket = requestSocket;
         this.clientID = clientNumber;
     }
 
     @Override
     public void run() {
         try {
-            
-            DataInputStream dataIn   = new DataInputStream(
-                socket.getInputStream()
-            );
-            DataOutputStream dataOut = new DataOutputStream(
-                socket.getOutputStream()
+
+            dataIn = new DataInputStream(
+                    socket.getInputStream()
             );
 
-            String line;
-            boolean done = false;
+            dataOut = new DataOutputStream(
+                    socket.getOutputStream()
+            );
 
+            boolean listen = true;
 
-        } catch(IOException e) {
+            while (listen) {
+                try {
+                    String line = dataIn.readUTF();
+                    System.out.println(line);
+
+                    // Tell the server to send this message to all the other clients
+                    Server.messageClients(this, line);
+                } catch (IOException ioe) {
+                    listen = false;
+                }
+            }
+
+        } catch (IOException e) {
             System.out.println(String.format("Client %d error %s", this.clientID, e.toString()));
         } finally {
             try {
                 socket.close();
-            } catch(Exception e) {
-                System.out.println("Socket close error"+e.toString());
+            } catch (Exception e) {
+                System.out.println("Socket close error" + e.toString());
             }
         }
     }
 
-    
+    /**
+     * Sends a message to all the clients currently connected
+     *
+     * @param msg - The message which will be sent
+     */
+    public void sendMessage(String msg) throws IOException {
+        System.out.println("DEBUG: Writing message UTF");
+
+        dataOut.writeUTF(msg);
+        dataOut.flush();
+    }
 }
