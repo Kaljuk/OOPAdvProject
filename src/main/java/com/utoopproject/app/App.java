78dc95f0680e059e7b9d61ec9ea0f02d7910729e
Erik K <erik.kaljumae@gmail.com>
2018-04-23 11:41:23 +0300

    Added method 2 and improved scanner imput flow

--- src/main/java/com/utoopproject/app/App.java
+++ src/main/java/com/utoopproject/app/App.java
@@ -13,34 +13,27 @@ public class App {
         System.out.println("1. launch server");
         System.out.println("2. launch client");
 
-        System.out.println(scannerInt(sc));
-
         // Check the input and launch the selected option
-        if (sc.hasNextInt()) {
-            
-            int option = sc.nextInt();
+        // Check input
+        int appChoice = scannerInt(sc);
+        
+        if (appChoice == 1) {
+            Server server = new Server();
+            server.startServer();
+        } else if (appChoice == 2) {
+            sc.nextLine();
 
-            if (option == 1) {
-                Server server = new Server();
-                server.startServer();
-            } else if (option == 2) {
-                sc.nextLine();
+            // Ask the user for server IP address
+            System.out.println("Enter the server IP address: ");
+            String serverAddress = sc.nextLine();
 
-                // Ask the user for server IP address
-                System.out.println("Enter the server IP address: ");
-                String serverAddress = sc.nextLine();
+            // Ask the user for username
+            System.out.println("Enter your username: ");
+            String username = sc.nextLine();
 
-                // Ask the user for username
-                System.out.println("Enter your username: ");
-                String username = sc.nextLine();
-
-                // Start a new client
-                Client client = new Client(serverAddress, username);
-                // Client.start(); ?
-
-            } else {
-                System.out.println("Invalid option!");
-            }
+            // Start a new client
+            Client client = new Client(serverAddress, username);
+            // Client.start(); ?
         }
     }
 
@@ -58,6 +51,25 @@ public class App {
             } catch (NumberFormatException e) {
                 gotInt     = false;
                 System.out.print("\nIntegers only, please try again: ");
+            }
+        }
+        return scannedInt;
+    }
+    /**
+     * @param  scan - Scanner entity
+     * @param  errorMsg - Message to show when user input is different from needed
+     * @return Integer as from scanner
+     */
+    private static int scannerInt(Scanner scan, String errorMsg) {
+        boolean gotInt = false;
+        int scannedInt = 0;
+        while (!gotInt) {
+            try {
+                scannedInt = Integer.valueOf(scan.nextLine());
+                gotInt     = true;
+            } catch (NumberFormatException e) {
+                gotInt     = false;
+                System.out.print(errorMsg);
             }
         }
         return scannedInt;
